5a1f29a3b8a476e8e6c7b9cde8ed6e31eb0dc9a8
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-12-13 08:04:34 +0530

    add scroll view to data table

--- components/data_table/data-table-row-actions.tsx
+++ components/data_table/data-table-row-actions.tsx
@@ -1,89 +1,26 @@
 "use client"
 
-import { DotsHorizontalIcon } from "@radix-ui/react-icons"
 import { Row } from "@tanstack/react-table"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { usePathname } from "next/navigation"
-import { useSession } from "next-auth/react"
-import { useToast } from "../ui/use-toast"
+import { UserActions } from "./actions/user_actions"
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
 }
 
-
-const deleteUser = async (id:string, session: any) => {
-
-  const response = await fetch(`http://localhost:8080/api/v1/o/${session.user.organization_id}/users/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${session.id_token}`
-    }
-  });
-
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-};
-
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
-  const { data: session } = useSession()
-  const pathname = usePathname()
-  const { toast } = useToast()
-  const handleDelete = async () => {
-    switch(pathname) {
-      case '/users':
-        try {
-          await deleteUser((row.original as any).id, session)
-          toast({
-            title: "User Deleted Successfully",
-            description: 'The user has been deleted successfully.',
-          })
-        } catch (error) {
-          toast({
-            title: "User Deletion Failed",
-            description: error instanceof Error ? error.message : 'Error while deleting the user',
-          })
-        }
-      default :
-        
-    }
-  }
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-        >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuItem>
-          <button onClick={handleDelete}>Delete</button>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+  const pathname = usePathname()
+  switch(pathname) {
+    case '/users':
+        return (
+          <UserActions row={row}/>
+        )
+    default :
+      return (
+        <div></div>
+      )
+  }
 }
